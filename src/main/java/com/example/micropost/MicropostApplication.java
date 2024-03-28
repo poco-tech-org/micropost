753b9cea8ab2ab5155d411739bcbe82e95b078c1
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-03-28 23:00:19 +0900

    first commit

---
+++ src/main/java/com/example/micropost/MicropostApplication.java
@@ -0,0 +1,13 @@
+package com.example.micropost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicropostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicropostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/micropost/MicropostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.micropost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicropostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
